c51c6274c76a2a46285c2b26256e118f29c8762c
Cristovao Duarte Sousa <crisjss@gmail.com>
2013-08-12 13:20:20 +0100

    Improve organization of functionality

--- lmi_sdp/lm.py
+++ lmi_sdp/lm.py
@@ -11,7 +11,7 @@ class NonLinearMatrixError(ValueError):
     pass
 
 
-def get_diag_blocks(M):
+def get_diag_block_idxs(M):
     """Get major indexes of diagonal blocks of squared matrices"""
     if M.shape[0] != M.shape[1]:
         raise NonSquareMatrixError('matrix must be square')
@@ -28,76 +28,49 @@ def get_diag_blocks(M):
     return b + [n]
 
 
-def lm_sym_to_coeffs(linear_matrix, variables, split_diag_blocks=False):
+def split_by_diag_blocks(matrix):
+    """Split a squared matrix into its diagonal blocks"""
+    idxs = get_diag_block_idxs(matrix)
+    blocks = [matrix[a:b, a:b] for (a, b) in zip([0]+idxs[:-1], idxs)]
+    return blocks
+
+
+def lm_sym_to_coeffs(linear_matrix, variables):
     """Convert a symbolic matrix linear w.r.t. variables into a list of
     numerical coefficient matrices"""
     LM = linear_matrix
     X = variables
     nx = len(X)
 
-    if isinstance(LM, list):
-        LM_blocks = LM
-    elif isinstance(LM, Matrix):
-        if split_diag_blocks:
-            blocks = get_diag_blocks(LM)
-            LM_blocks = []
-            for a, b in zip([0]+blocks[:-1], blocks):
-                LM_blocks.append(LM[a:b, a:b])
-        else:
-            LM_blocks = [LM]
-
-    coeffs_blocks = []
-
     one = S(1)
     ok_set = set(X) | set([one])
-    for LM_b in LM_blocks:
-        consts = np.zeros((LM_b.rows, LM_b.cols))
-        coeffs = [np.zeros((LM_b.rows, LM_b.cols)) for i in range(nx)]
-        for elem in [(i, j) for i in range(LM_b.rows)
-                     for j in range(LM_b.cols)]:
-            expr = LM_b[elem]
+    consts = np.zeros((LM.rows, LM.cols))
+    coeffs = [np.zeros((LM.rows, LM.cols)) for i in range(nx)]
+    for elem in [(i, j) for i in range(LM.rows) for j in range(LM.cols)]:
+        expr = LM[elem]
+        coeff_dict = expr.as_coefficients_dict()
+        if not set(coeff_dict.keys()).issubset(ok_set):
+            expr = expr.expand()  # try expanding
             coeff_dict = expr.as_coefficients_dict()
             if not set(coeff_dict.keys()).issubset(ok_set):
-                expr = expr.expand()  # try expanding
-                coeff_dict = expr.as_coefficients_dict()
-                if not set(coeff_dict.keys()).issubset(ok_set):
-                    raise NonLinearMatrixError(
-                        "'linear_matrix' must be composed of linear "
-                        "expressions w.r.t. 'variables'")
-            consts[elem] = coeff_dict.get(one, 0)
-            for i, x in enumerate(X):
-                coeffs[i][elem] = coeff_dict.get(x, 0)
-        coeffs_blocks.append((coeffs, consts))
+                raise NonLinearMatrixError(
+                    "'linear_matrix' must be composed of linear "
+                    "expressions w.r.t. 'variables'")
+        consts[elem] = coeff_dict.get(one, 0)
+        for i, x in enumerate(X):
+            coeffs[i][elem] = coeff_dict.get(x, 0)
 
-    if isinstance(LM, list) or split_diag_blocks:
-        return coeffs_blocks
-    else:
-        return coeffs_blocks[0]
+    return (coeffs, consts)
 
 
-def lm_coeffs_to_sym(coeffs, variables, join_blocks=False):
+def lm_coeffs_to_sym(coeffs, variables):
     """Create a symbolic matrix linear w.r.t. variables given a list of
     numerical coefficient matrices"""
     X = variables
     nx = len(X)
 
-    if isinstance(coeffs, list):
-        coeffs_blocks = coeffs
-    else:
-        coeffs_blocks = [coeffs]
+    LM = Matrix(coeffs[1])
+    for i, x in enumerate(X):
+        LM += x*Matrix(coeffs[0][i])
 
-    LM_blocks = []
-
-    for _coeffs in coeffs_blocks:
-        LM_block = Matrix(_coeffs[1])
-        for i, x in enumerate(X):
-            LM_block += x*Matrix(_coeffs[0][i])
-        LM_blocks.append(LM_block)
-
-    if isinstance(coeffs, list):
-        if join_blocks:
-            return diag(*LM_blocks)
-        else:
-            return LM_blocks
-    else:
-        return LM_blocks[0]
+    return LM
--- lmi_sdp/tests/test_lm.py
+++ lmi_sdp/tests/test_lm.py
@@ -1,19 +1,32 @@
 from sympy import Matrix
 import numpy as np
-from lmi_sdp import get_diag_blocks, NonSquareMatrixError
+from lmi_sdp import NonSquareMatrixError, get_diag_block_idxs, split_by_diag_blocks
 
 
-def test_get_diag_blocks():
+def test_get_diag_block_idxs():
     m = Matrix([[1, 0, 0], [0, 2, 3], [0, 3, 4]])
-    assert get_diag_blocks(m) == [1, 3]
+    assert get_diag_block_idxs(m) == [1, 3]
     m = np.matrix([[1, 0, 0], [0, 2, 3], [0, 3, 4]])
-    assert get_diag_blocks(m) == [1, 3]
+    assert get_diag_block_idxs(m) == [1, 3]
     m = Matrix([[0, 1, 0], [0, 0, 0], [0, 0, 0]])
-    assert (get_diag_blocks(m), get_diag_blocks(m.T)) == ([2, 3], [2, 3])
+    assert (get_diag_block_idxs(m), get_diag_block_idxs(m.T)) == ([2, 3], [2, 3])
     m = Matrix([[1, 0, 0], [0, 1, 1]])
     try:
-        get_diag_blocks(m)
+        get_diag_block_idxs(m)
         except_ok = False
     except NonSquareMatrixError:
         except_ok = True
     assert except_ok
+
+
+def test_split_by_diag_blocks():
+    m = Matrix([[1, 0, 0], [0, 2, 3], [0, 3, 4]])
+    assert split_by_diag_blocks(m) == \
+        [Matrix([[1]]), Matrix([[2, 3], [3, 4]])]
+    m = np.matrix([[1, 0, 0], [0, 2, 3], [0, 3, 4]])
+    mb = split_by_diag_blocks(m)
+    assert mb[0] == np.matrix([[1]])
+    assert (mb[1] == np.matrix([[2, 3], [3, 4]])).all()
+    m = Matrix([[0, 1, 0], [0, 0, 0], [0, 0, 0]])
+    assert split_by_diag_blocks(m) == \
+        [Matrix([[0, 1], [0, 0]]), Matrix([[0]])]
